8b915e1fadd86fe5d701d52dc58729a43bc1be15
abhaygupta08 <abhay.19201@knit.ac.in>
2021-06-18 13:31:52 +0530

    day8Push

---
+++ 5. FRONTEND/DAY 8/task2/script.js
@@ -0,0 +1,29 @@
+window.onload = function(){
+// console.log(sq)
+const row = document.createElement("div");
+row.classList.add("row");
+const sq = document.createElement("div");
+sq.classList.add("sq","sq-white");
+sq.setAttribute("onclick","toggleColor(this)");
+for(let i =0;i<10;i++){
+    row.innerHTML += sq.outerHTML;
+}
+const bigSq = document.querySelector(".bigSq");
+    for (let i = 0; i < 10; i++) {
+    bigSq.innerHTML += row.outerHTML;
+    }
+const sqr = document.querySelectorAll(".sq");
+
+for(let i = 0;i<60;i++){
+    let index = Math.floor(Math.random()*100);
+    
+    sqr[index].classList.toggle("sq-white");
+    sqr[index].classList.toggle("sq-red");
+}
+
+}
+
+const toggleColor = function (event) {
+    event.classList.toggle("sq-white");
+    event.classList.toggle("sq-red");
+}
