b60512b29425d86065c0db748fc6fd99c4da112a
abhaygupta08 <abhay.19201@knit.ac.in>
2021-07-01 21:01:21 +0530

    all done

--- 5. FRONTEND/Day 10/script.js
+++ 5. FRONTEND/Day 10/script.js
@@ -1,72 +1,55 @@
-let cardIsFlipped = false;
-let boardLocked = false;
-let firstCard;
-let secondCard;
+const cards = document.querySelectorAll(".memory-card");
+// console.log(cards);
 
-const card = document.querySelectorAll(".card");
-// console.log(card)
-function SetColor(){
-    card[0].classList.add("yellow")
-    card[1].classList.add("violet")
-    card[2].classList.add("blue")
-    card[3].classList.add("maroon")
-    card[4].classList.add("orange")
-    card[5].classList.add("green")
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard,secondCard;
 
-    card[6].classList.add("yellow")
-    card[7].classList.add("violet")
-    card[8].classList.add("blue")
-    card[9].classList.add("maroon")
-    card[10].classList.add("orange")
-    card[11].classList.add("green")
 
-}
-function hideColor(){
-    for(let i=0;i<card.length;i++){
-        card[i].classList.add("hide")
-    }
-}
-SetColor();
-hideColor();
-
-function resetCards() {
-    cardIsFlipped = false;
-    boardLocked = false;
-    firstCard = null
-    secondCard = null;
-}
-
-function disableCard(){
-    firstCard.removeEventListener("click",flipCards)
-    secondCard.removeEventListener("click",flipCards)
-    resetCards()
-}
-function unflipCards(){
-    boardLocked = true;
-    firstCard.classList.toggle("hide")
-    secondCard.classList.toggle("hide")
-    restcard()
-}
-function flipCards() {
-    // console.log(this,firstCard,secondCard)
-    if (boardLocked) return;
-    if (this == firstCard) return;
-    this.classList.toggle("hide")
-
-    if (!cardIsFlipped) {
-        cardIsFlipped = true;
+function flipCard() {
+    if(lockBoard) return;
+    if(this==firstCard) return;
+    this.classList.add("flip");
+    if(!hasFlippedCard){
+        hasFlippedCard = true;
         firstCard = this;
-        console.log(this.style)
-        return;
     }
-    secondCard = this;
-    doMatch();
+    else{
+        hasFlippedCard = false;
+        secondCard = this;
+        // console.log(firstCard.dataset.color, secondCard.dataset.color);
+        if(firstCard.dataset.color===secondCard.dataset.color){
+            firstCard.removeEventListener('click', flipCard);
+            secondCard.removeEventListener('click', flipCard);
+            resetCards();
+
+        }
+        else{
+            lockBoard = true;
+            setTimeout(() => {
+                firstCard.classList.remove('flip')
+                secondCard.classList.remove('flip')
+                lockBoard = false;
+                resetCards();
+            }, 1000);
+        }
+    }
+
 }
 
-card.forEach(card => card.addEventListener("click",flipCards) )
-function doMatch(){
-    let matches  = firstCard.style.background == secondCard.style.background;
-    if(matches){disableCard();}
-    else{unflipCards()}
-    
+function resetCards(){
+    [hasFlippedCard,lockBoard] = [false,false];
+    [firstCard,secondCard] = [null,null];
 }
+
+(function shuffleCards(){
+    cards.forEach(card => {
+        let randomPos = Math.floor(Math.random() * 8);
+        card.style.order = randomPos;
+    })
+})();
+
+cards.forEach(card => {
+    card.addEventListener('click',flipCard);
+});
+
